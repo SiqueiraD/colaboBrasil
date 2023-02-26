24e6c8770b65e1924f2101b92ad67e513de0d32d
Danilo Siqueira <danilo_siqueira@id.uff.br>
2023-02-25 22:01:12 -0300

    teste de funcoes serveless vercel

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import axios, { AxiosResponse } from 'axios'
+import parseAPI from '@/services/parseAPI'
 
 type Data = {
   statusCode: number
@@ -11,19 +12,31 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const response = await axios
-      .request({
-        url: `https://api.agify.io?name=michael`,
-        method: 'get',
-      })
-      .then((resp: AxiosResponse) => ({
-        statusCode: resp.status,
-        body: resp.data,
-      }))
-      .catch((error: any) => ({
-        statusCode: error.response.status,
-        body: error.response.data,
-      }))
+  // const response = await axios
+  //     .request({
+  //       url: `https://api.agify.io?name=michael`,
+  //       method: 'get',
+  //     })
+  //     .then((resp: AxiosResponse) => ({
+  //       statusCode: resp.status,
+  //       body: resp.data,
+  //     }))
+  //     .catch((error: any) => ({
+  //       statusCode: error.response.status,
+  //       body: error.response.data,
+  //     }))
+  const response = await parseAPI.post('users', { username: 'username', password: '123' }, undefined, {
+    'X-Parse-Revocable-Session': 1,
+    'Content-Type': 'application/json'
+  })
+    .then((resp: AxiosResponse) => ({
+      statusCode: resp.status,
+      body: resp.data,
+    }))
+    .catch((error: any) => ({
+      statusCode: error.response.status,
+      body: error.response.data,
+    }))
 
   res.status(200).json(response)
 }
--- pages/api/user/auth.ts
+++ pages/api/user/auth.ts
@@ -1,6 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import axios, { AxiosResponse } from 'axios'
 import parseAPI from '@/services/parseAPI'
 
 export default async function handler(
